0ef7949a36abe06a30fbebf41c95d5c0e87813bc
Devan-M316 <dmathis316@lsr7.net>
2023-09-25 09:58:02 -0500

    added card gifs but need fixes to file so its inside blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,15 @@
 # Devan Mathis, Miles Camey, Mason Percival, Nathaniel Drake
 # 09/06/23
 
-# use turtle to make individiual cards with each of them as turtles
+import turtle as t
+wn = t.Screen()
+# 540 x 360
+
+t.bgpic("green_felt_bg.gif")
+
+card_back = t.Turtle()
+wn.addshape('card_back.gif')
+
+card_back.shape('card_back.gif')
+
+t.mainloop()
